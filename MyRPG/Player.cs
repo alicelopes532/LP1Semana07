0290865f62d9860556fda5d0bb00b0469582336c
Alice <alicelopess.art@gmail,com>
2025-04-04 14:54:30 +0100

    projeto MyRPG

---
+++ MyRPG/Player.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Intrinsics.X86;
+using System.Security;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace 
+{
+    public class Player
+    {
+        private readonly int XP;
+        public class XP
+            public XP(int player.XP);
+            {
+                this.xp = player.XP;
+            }
+        private readonly float Health;
+
+        public class Health
+        {
+            public Health(float player.Health);
+            {
+                this.health = player.Health;
+            }
+        }
+        public class Name
+        {
+            public string Name{ get;}
+            public Player(string player.Name);
+            {
+                Name = player.Name;
+            }
+        }
+        
+            public int XP {get; set;} = player.MaxHealth;
+
+        public class Levelxp
+        {
+            public int Level {get;}
+        
+            public Levelxp(int player.Level)
+                {
+                    Levelxp = player.level;
+                }
+        }
+        public class HealthValue
+        {
+            public float HealthValue{get;}
+
+            public HealthValue(float player.MaxHealth)
+            {
+                HealthValue = player.MaxHealth;
+            }
+        }
+
+        public void TakeDamage()
+
+    }
+}
